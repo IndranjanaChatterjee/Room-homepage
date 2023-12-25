130c71da2c57921c01843d3594b594e9b58dc87c
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-12-25 18:59:43 +0530

    nav_bar

--- room/src/Components/Nav.jsx
+++ room/src/Components/Nav.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Nav() {
   return (
     <header>
-      <nav className="bg-blue-50">
+      <nav className="bg-blue-50 flex flex-row justify-start gap-[1.5rem] p-[1.5rem]">
         <span className="home">Home</span>
         <span className="list">
-          <ul>
+          <ul className="flex flex-row justify-start gap-[0.75rem]">
             <li>Home</li>
             <li>Shop</li>
             <li>About</li>
